1087b50b1e6230a7fb1e5a0a80d51d9b544ef09a
Mateus Franchini de Freitas <contato.mateusfr@outlook.com>
2021-10-22 09:16:23 -0300

    Middlewares

--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/MateusFrFreitas/goland-gin-poc/controller"
+	"github.com/MateusFrFreitas/goland-gin-poc/middleware"
 	"github.com/MateusFrFreitas/goland-gin-poc/service"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -13,8 +17,17 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, videoController.FindAll())
